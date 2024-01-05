a1e15879b6e95d1d8c2378a6e4fbf8bbc431d555
sukidayo <2985384723@qq.com>
2024-01-05 21:59:12 +0800

    更新笔记

--- src/test/java/com/cnsukidayo/hibernate/JPATest.java
+++ src/test/java/com/cnsukidayo/hibernate/JPATest.java
@@ -35,4 +35,45 @@ public class JPATest {
         transaction.commit();
     }
 
+    @Test
+    public void testR() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        // 懒加载查询
+        Customer customer = entityManager.getReference(Customer.class, 1L);
+
+        transaction.commit();
+    }
+
+    @Test
+    public void testD() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        /*
+        无法删除处于游离状态的实例
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+        */
+        // 只有从数据库中查出的实例才可以被删除
+        Customer customer = entityManager.find(Customer.class, 1L);
+        entityManager.remove(customer);
+
+        transaction.commit();
+    }
+
+    @Test
+    public void testCache() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Customer customer0 = entityManager.find(Customer.class, 1L);
+        Customer customer1 = entityManager.find(Customer.class, 1L);
+
+        transaction.commit();
+    }
+
+
 }
