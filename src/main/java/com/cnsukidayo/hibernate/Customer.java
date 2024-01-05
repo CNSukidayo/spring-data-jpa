f369c007670c74965418ca0602d62bc332d1a54d
guyuanjie <guyuanjie@yiqicai.com>
2024-01-05 16:57:46 +0800

    first commit

---
+++ src/main/java/com/cnsukidayo/hibernate/Customer.java
@@ -0,0 +1,47 @@
+package com.cnsukidayo.hibernate;
+
+import javax.persistence.*;
+
+/**
+ * @author guyuanjie
+ * @date 2024/1/5 16:50
+ */
+@Entity
+@Table(name = "tb_customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_address")
+    private String customerAddress;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+}
